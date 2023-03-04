b6a30686703580bd18bfa606df2009053c5b809a
mathieu perron <mathieu.perron95@outlook.com>
2023-03-03 21:45:33 -0500

    Answer to second advent of code challenge

--- aoc1/aoc1.py
+++ aoc1/aoc1.py
@@ -12,7 +12,15 @@ with open('input.txt', 'r') as f:
         result.append(sum)
 print(result)
 
+sum = 0
+
 # get the index of the highest value of the list
 max_index = result.index(max(result))
 print(max_index)
 print(result[max_index])
+# repeat this process three times to get the sum of the three highest values
+for i in range(3):
+    sum += result[max_index]
+    result.pop(max_index)
+    max_index = result.index(max(result))
+print(sum)
